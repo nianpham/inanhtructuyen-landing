52d6c354a6ae6c34c46cbcf598075510e4b8e26c
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-06-21 12:31:51 +0700

    fix: update order status

--- src/modules/tai-khoan/components/section-01.tsx
+++ src/modules/tai-khoan/components/section-01.tsx
@@ -172,8 +172,8 @@ const Section01 = () => {
           statusBank === "PAID"
             ? "paid"
             : statusBank === "CANCELLED"
-            ? "pending"
-            : "pending",
+            ? "waiting"
+            : "waiting",
         isPayed: statusBank === "PAID" ? true : false,
       };
       const order = await OrderService.updateOrder(orderIDBank, body);
